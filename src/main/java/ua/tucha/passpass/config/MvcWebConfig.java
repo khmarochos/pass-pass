8e95329259181695aa9d13d241da21eda4543180
v.melnik <v.melnik@tucha.ua>
2019-02-04 00:42:27 +0200

    Add the INSPINIA theme

---
+++ src/main/java/ua/tucha/passpass/config/MvcWebConfig.java
@@ -0,0 +1,26 @@
+package ua.tucha.passpass.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Configuration
+@EnableWebMvc
+public class MvcWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+        log.debug("ResourceHandlerRegistry is {}", registry);
+    }
+
+}
